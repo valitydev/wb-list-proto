4344b4f096338924354c91fc5b908ce9ceb21ffa
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-01-15 11:35:55 +0300

    Delete manage methods, must be not hear

--- proto/wb_list.thrift
+++ proto/wb_list.thrift
@@ -8,28 +8,10 @@ namespace erlang wb_list
 typedef string ID
 typedef string Value
 
-struct Info {
-    // ID party
-    1: required ID party_id;
-    // ID  магазина
-    2: required ID shop_id;
-    // Идентификатор списка
-    3: required ID list_name;
-    // Значение в списке
-    4: required Value value;
-}
-
-/**
- * Набор записей списка.
-**/
-struct InfoStorage {
-    1: required list<Info> infos
-}
-
 exception ListNotFound {}
 
 /**
-* Интерфейс для управления чёрными и белыми списками
+* Интерфейс для проверки по черным и белым спискам
 */
 service WbListService {
 
@@ -40,25 +22,4 @@ service WbListService {
     bool isExist(1: ID party_id, 2: ID shop_id, 3: ID list_name, 4: Value value)
         throws (1: ListNotFound ex1)
 
-    /**
-    * Добавление в список
-    * если списка не существует то выбрасывается ListNotFound
-    **/
-    void add(1: ID party_id, 2: ID shop_id, 3: ID list_name, 4: Value value)
-        throws (1: ListNotFound ex1)
-
-    /**
-    * Удаление из списка
-    * если списка не существует то выбрасывается ListNotFound
-    **/
-    void remove(1: ID party_id, 2: ID shop_id, 3: ID list_name, 4: Value value)
-        throws (1: ListNotFound ex1)
-
-    /**
-    * Постраничное получение списка
-    * если списка не существует то выбрасывается ListNotFound
-    * если запись в списке не найдена то возвращается пустой список
-    **/
-    InfoStorage getByPage(1: ID party_id, 2: ID shop_id, 3: ID list_name, 4: i32 offset 5: i32 size)
-        throws (1: ListNotFound ex1)
 }
